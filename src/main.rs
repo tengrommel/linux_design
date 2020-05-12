770d60e1d8f4844f6772fa6c4424bc13d5a92881
teng <wwwsousoucom@126.com>
2020-05-12 10:28:32 +0800

    log init

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 use dotenv;
 
 #[async_std::main]
-async fn main() {
+async fn main() ->Result<(),Error>{
     dotenv::dotenv().ok();
-    let db_url = std::env::var("DATABASE_URL").unwrap();
-    dbg!(db_url);
-    println!("hello world!");
+    pretty_env_logger::init();
+    let mut app = tide::new();
+    app.at("/").get(|_| async move {Ok("Hello, world!")});
+    app.listen("127.0.0.1:8000").await?;
+    Ok(())
+}
+
+#[derive(thiserror::Error, Debug)]
+enum Error {
+    #[error(transparent)]
+    DbError(#[from] sqlx::Error),
+    #[error(transparent)]
+    IoError(#[from] std::io::Error)
 }
