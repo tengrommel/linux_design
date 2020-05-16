49a32d31dd3835ed2df83df6bc8617b3a3de2ad0
teng <wwwsousoucom@126.com>
2020-05-16 20:49:14 +0800

    branch

--- ex/cli/src/main.rs
+++ ex/cli/src/main.rs
@@ -1,22 +1,59 @@
 // some-old-feature
 // what do to?(k/d/s/?) > s
+use std::fmt;
 use std::io;
 use std::io::{Read, Write};
-fn main() {
-    crossterm::terminal::enable_raw_mode().unwrap();
+use git2::Repository;
+fn main() -> Result<(), Error> {
+    let repo = Repository::open_from_env()?;
     let mut stdout = io::stdout();
-    let mut stdin = io::stdin().bytes();
-
-    loop {
-        write!(stdout, "Type something >").unwrap();
-        stdout.flush().unwrap();
-        let c = char::from(stdin.next().unwrap().unwrap());
-        if c == 'q' {
-            break;
-        }
-        write!(stdout, "You typed '{}'\n\r", c).unwrap();
-        stdout.flush().unwrap();
+    for branch in repo.branches(None)? {
+        let (branch, branch_type) = branch?;
+        let name = branch.name_bytes()?;
+        stdout.write_all(name)?;
+        write!(stdout, "\n");
     }
-
-    crossterm::terminal::disable_raw_mode().unwrap();
+    Ok(())
 }
+
+#[derive(Debug, thiserror::Error)]
+enum Error {
+    #[error(transparent)]
+    CrosstermError(#[from] crossterm::ErrorKind),
+    
+    #[error(transparent)]
+    IoError(#[from] io::Error),
+
+    #[error(transparent)]
+    GitError(#[from] git2::Error)
+}
+
+// impl From<crossterm::ErrorKind> for Error {
+//     fn from(error: crossterm::ErrorKind) -> Error {
+//         Error::CrosstermError(error)
+//     }
+// }
+
+// impl From<io::Error> for Error {
+//     fn from(error: io::Error) -> Error {
+//         Error::IoError(error)
+//     }
+// }
+
+// impl fmt::Display for Error {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         match self {
+//             Error::CrosstermError(inner) => write!(f, "{}", inner),
+//             Error::IoError(inner) => write!(f, "{}", inner),
+//         }
+//     }
+// }
+
+// impl std::error::Error for Error {
+//     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+//         match self {
+//             Error::CrosstermError(inner) => Some(inner),
+//             Error::IoError(inner) => Some(inner),
+//         }
+//     }
+// }
