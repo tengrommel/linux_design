d1c653b91510e465301552758fd48ec356abfb48
teng <wwwsousoucom@126.com>
2020-05-17 00:08:33 +0800

    file

--- ex/fit/src/main.rs
+++ ex/fit/src/main.rs
@@ -1,1 +1,16 @@
-fn main() {}
+use std::fs::File;
+use std::path::Path;
+use std::error::Error;
+use std::io::Read;
+fn main() {
+    let data_path = Path::new("data/HRSz0yd020fm_c2f.fits");
+    let mut file = match File::open(data_path) {
+        Err(e) => panic!("Couldn't open {}: {}", data_path.display(), e.description()),
+        Ok(file) => file,
+    };
+    let mut fits_data = Vec::new();
+    match file.read_to_end(&mut fits_data) {
+        Err(e) => panic!("Couldn't read file: {}: {}", data_path.display(), e.description()),
+        Ok(bytes) => println!("Successfully read {} bytes from {}", bytes, data_path.display()),
+    };
+}
