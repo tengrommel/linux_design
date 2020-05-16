ac13d43f30626d9802f477d6ad2f149e083626f3
teng <wwwsousoucom@126.com>
2020-05-16 18:04:16 +0800

    first

--- ex/cli/src/main.rs
+++ ex/cli/src/main.rs
@@ -1,1 +1,22 @@
-fn main() {}
+// some-old-feature
+// what do to?(k/d/s/?) > s
+use std::io;
+use std::io::{Read, Write};
+fn main() {
+    crossterm::terminal::enable_raw_mode().unwrap();
+    let mut stdout = io::stdout();
+    let mut stdin = io::stdin().bytes();
+
+    loop {
+        write!(stdout, "Type something >").unwrap();
+        stdout.flush().unwrap();
+        let c = char::from(stdin.next().unwrap().unwrap());
+        if c == 'q' {
+            break;
+        }
+        write!(stdout, "You typed '{}'\n\r", c).unwrap();
+        stdout.flush().unwrap();
+    }
+
+    crossterm::terminal::disable_raw_mode().unwrap();
+}
