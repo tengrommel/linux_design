5b05e1719bb2463d5349e2c994b1f12816ce3713
teng <wwwsousoucom@126.com>
2020-05-22 17:11:01 +0800

    ok

--- ex/fit/src/main.rs
+++ ex/fit/src/main.rs
@@ -11,6 +11,26 @@ enum Section {
     Extension,
 }
 
+struct Fits<Header, Data, Extension> {
+    header: Header,
+    data: Data,
+    extension: Vec<Extension>,
+}
+
+#[derive(Default)]
+struct Header {
+    data: HashMap<String, String>,
+}
+
+#[derive(Default)]
+struct Data<'t> {
+    data: &'t [u8],
+}
+
+struct Extension {
+    data: String,
+}
+
 struct Record {
     key: String,
     value: String,
@@ -21,7 +41,6 @@ const RECORD_SIZE: usize = 80;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
     let file_name = if args.len() > 1 {
         args[1].to_string()
     } else {
@@ -41,50 +60,63 @@ fn main() {
     let mut state = Section::Header;
     let mut block_index = 0;
 
-    let _chunks: Vec<()> = fits_data
-        .chunks(BLOCK_SIZE)
-        .enumerate()
-        .map(|(i, chunk)| {
-            println!("Processing chunk, {}", i);
-            match state {
-                Section::Header => {
-                    if String::from_utf8_lossy(chunk).contains(" END ") {
-                        parse_header(&fits_data[block_index..(block_index + (i + 1) * BLOCK_SIZE)]);
-                        state = Section::Data;
-                        block_index = i;
-                    }
+    let mut fits: Fits<Header, Data, Extension> = Fits {
+        header: Default::default(),
+        data: Default::default(),
+        extension: Default::default(),
+    };
+
+    for (current_block, chunk) in fits_data.chunks(BLOCK_SIZE).enumerate() {
+        println!("Processing chunk, {}", current_block);
+        match state {
+            Section::Header => {
+                if String::from_utf8_lossy(chunk).contains(" END ") {
+                    fits.header = parse_header(&fits_data, block_index, current_block);
+                    state = Section::Data;
+                    block_index = current_block;
                 }
-                Section::Data => {
-                    let data_size =
-                        parse_data(&fits_data[block_index..(block_index + (i + 1) * BLOCK_SIZE)]);
+            }
+            Section::Data => {
+                let dimensionality = 2;
+                if dimensionality == 2 {
+                    let (x, y) = (200, 2);
+                    fits.data = parse_data(&fits_data, block_index, (x, y));
+                }
+                state = Section::Extension;
+                block_index = current_block + (fits.data.data.len() / BLOCK_SIZE);
+            }
+            Section::Extension => {
+                if String::from_utf8_lossy(chunk).contains("XTENSION") {
+                    println!("[Found extension start]");
                     state = Section::Extension;
-                    block_index = i;
                 }
-                Section::Extension => {
-                    if String::from_utf8_lossy(chunk).contains("XTENSION") {
-                        println!("[Found extension start]");
-                        state = Section::Extension;
-                    }
-                    // @todo it seems possible to hit this block and try and parse
-                    if String::from_utf8_lossy(chunk).contains(" END ") {
-                        parse_extension(
-                            &fits_data[block_index..(block_index + (i + 1) * BLOCK_SIZE)],
-                        );
-                        state = Section::Header;
-                        block_index = i;
-                    }
+                // @todo it seems possible to hit this block and try and parse
+                if String::from_utf8_lossy(chunk).contains(" END ") {
+                    fits.extension
+                        .push(parse_extension(&fits_data, block_index, current_block));
+                    state = Section::Header;
+                    block_index = current_block;
                 }
-            };
-        })
-        .collect();
+            }
+        };
+    }
 }
 
-fn parse_data(data: &[u8]) -> usize {
-    0
-}
-
-fn parse_extension(data: &[u8]) {
-    println!("[Parsing data]");
+fn parse_header(fits: &[u8], last_block: usize, current_block: usize) -> Header {
+    let mut header_records = HashMap::new();
+    let header_data =
+        &fits[(last_block * BLOCK_SIZE)..(last_block + (current_block + 1) * BLOCK_SIZE)];
+    println!("Found header end");
+    for chunk in header_data.chunks(RECORD_SIZE) {
+        let record_string = String::from_utf8_lossy(chunk);
+        if let Some(Record { key, value }) = parse_record(record_string) {
+            println!("{}: {}", key, value);
+            header_records.insert(key, value);
+        }
+    }
+    Header {
+        data: header_records,
+    }
 }
 
 fn parse_record(record: Cow<str>) -> Option<Record> {
@@ -100,19 +132,20 @@ fn parse_record(record: Cow<str>) -> Option<Record> {
     }
 }
 
-fn parse_header(data: &[u8]) {
-    let mut header_records = HashMap::new();
-    println!("Found header end");
-    for (i, _) in data.iter().enumerate().step_by(RECORD_SIZE) {
-        let record = &data[i..(i + RECORD_SIZE)];
-        let record_string = String::from_utf8_lossy(record);
-        if let Some(Record { key, value }) = parse_record(record_string) {
-            // println!("{}: {}", key, value);
-            header_records.insert(key, value);
-        }
-    }
+fn parse_data(fits: &[u8], last_block: usize, (x, y): (u32, u32)) -> Data {
+    let data_size = x * y * 4;
+    let data_unit =
+        &fits[(last_block * BLOCK_SIZE)..(data_size as usize + (last_block * BLOCK_SIZE))];
+    Data { data: data_unit }
 }
 
+fn parse_extension(fits: &[u8], last_block: usize, current_block: usize) -> Extension {
+    println!("[Parsing data]");
+    let extension_data = &fits[(last_block * BLOCK_SIZE)..(current_block + 1) * BLOCK_SIZE];
+    Extension {
+        data: String::from_utf8_lossy(extension_data).to_string(),
+    }
+}
 // fn stringify(data: &[u8]) -> String {
 // String::from_utf8_lossy(data).into_owned()
 // }
