b0e3cd65dd6a0ea3087a653671cf2a731c22984c
teng <wwwsousoucom@126.com>
2020-05-24 17:42:07 +0800

    await

--- ex/stream/src/main.rs
+++ ex/stream/src/main.rs
@@ -8,8 +8,12 @@ use tide::Server;
 
 #[async_std::main]
 async fn main() {
+    let app = server().await;
+    app.listen("127.0.0.1:8000").await.unwrap();
+}
+
+async fn server() -> Server<State> {
     dotenv::dotenv().ok();
-    pretty_env_logger::init();
     let db_url = std::env::var("DATABASE_URL").unwrap();
     let db_pool: PgPool = Pool::new(&db_url).await.unwrap(); // 必须声明
     let mut app: Server<State> = Server::with_state(State { db_pool });
@@ -17,7 +21,7 @@ async fn main() {
         let json = json!([1, 2, 3]);
         Ok(Response::new(StatusCode::Ok).body_json(&json)?)
     });
-    app.listen("127.0.0.1:8000").await.unwrap();
+    app
 }
 
 #[derive(Debug)]
