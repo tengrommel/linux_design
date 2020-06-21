7955d6e2dc1657d816202957b22a4b87929aa79e
teng <wwwsousoucom@126.com>
2020-06-21 08:31:31 +0800

    command_line

---
+++ ex/command_line/target/debug/command_line.d
@@ -0,0 +1,1 @@
+/home/teng/CLionProjects/linux_design/ex/command_line/target/debug/command_line: /home/teng/CLionProjects/linux_design/ex/command_line/src/main.rs
---
+++ ex/command_line/target/debug/deps/command_line-3c497bd228e04a18.d
@@ -0,0 +1,5 @@
+/home/teng/CLionProjects/linux_design/ex/command_line/target/debug/deps/command_line-3c497bd228e04a18: src/main.rs
+
+/home/teng/CLionProjects/linux_design/ex/command_line/target/debug/deps/command_line-3c497bd228e04a18.d: src/main.rs
+
+src/main.rs:
